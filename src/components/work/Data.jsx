f681aa131b7fd21b398a45ddcdb6d373505f1c6c
RajeenKhadka <rajeenkhadka315@gmail.com>
2025-01-29 13:48:24 -0500

    Added the first project

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -1,25 +1,31 @@
-import Work1 from "../../assets/work1.jpg";
 import Work2 from "../../assets/work2.jpg";
 import Work3 from "../../assets/work3.jpg";
+import BakeryShop from "../../assets/BakeryShop.png";
 
 export const projectsData = [
   {
     id: 1,
-    image: Work1,
-    title: "Web design",
+    image: BakeryShop,
+    title: "Cake Bakery Fullstack",
     category: "web",
+    githubUrl: "https://github.com/RajeenKhadka/bakery-shop-fullstack.git",
+    siteUrl: "https://mayabakeryshop.netlify.app/",
   },
   {
     id: 2,
     image: Work2,
     title: "App movil",
     category: "app",
+    githubUrl: "",
+    siteUrl: "",
   },
   {
     id: 3,
     image: Work3,
     title: "Brand design",
     category: "design",
+    githubUrl: "",
+    siteUrl: "",
   },
 ];
 
--- src/components/work/WorkItems.jsx
+++ src/components/work/WorkItems.jsx
@@ -3,7 +3,10 @@ function WorkItems({ item }) {
     <div className="work__card" key={item.id}>
       <img src={item.image} alt="" className="work__img" />
       <h3 className="work__title">{item.title}</h3>
-      <a href="#" className="work__button">
+      <a href={item.githubUrl} className="work__button">
+        Github <i className="bx bx-right-arrow-alt work__button-icon"></i>
+      </a>
+      <a href={item.siteUrl} className="work__button">
         Demo <i className="bx bx-right-arrow-alt work__button-icon"></i>
       </a>
     </div>
