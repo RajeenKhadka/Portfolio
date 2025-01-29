de66f86f4a2398c203faf759a76da4b96f87a06f
RajeenKhadka <rajeenkhadka315@gmail.com>
2025-01-29 18:40:43 -0500

    Updated Works

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -1,14 +1,13 @@
-import Work2 from "../../assets/work2.jpg";
-import Work3 from "../../assets/work3.jpg";
 import BakeryShop from "../../assets/BakeryShop.png";
 import SchoolDBApp from "../../assets/SchoolDBApp.jpg";
 import CitadelOfRick from "../../assets/CitadelOfRick.jpg";
+import f12024hub from "../../assets/f12024hub.jpg";
 
 export const projectsData = [
   {
     id: 1,
     image: BakeryShop,
-    title: "Cake Bakery Fullstack",
+    title: "Cake Bakery Ecommerce - Fullstack",
     category: "web",
     githubUrl: "https://github.com/RajeenKhadka/bakery-shop-fullstack.git",
     siteUrl: "https://mayabakeryshop.netlify.app/",
@@ -16,7 +15,7 @@ export const projectsData = [
   {
     id: 2,
     image: SchoolDBApp,
-    title: "SchoolDB - CRUD & EJS-Powered",
+    title: "School DB - CRUD & EJS-Powered",
     category: "web",
     githubUrl:
       "https://github.com/RajeenKhadka/Assignment-SBA-319-MongoDB-Database-Application.git",
@@ -25,11 +24,19 @@ export const projectsData = [
   {
     id: 3,
     image: CitadelOfRick,
-    title: "CitadelOfRick - Characters API",
+    title: "Citadel Of Rick - Characters API",
     category: "web",
     githubUrl:
       "https://github.com/RajeenKhadka/SBA320H-ReactWebApplicationProject.git",
     siteUrl: "https://citadel-of-rick.netlify.app/",
+  },
+  {
+    id: 4,
+    image: f12024hub,
+    title: "F1 2024 - HTML, CSS & Bootstrap",
+    category: "web",
+    githubUrl: "https://github.com/RajeenKhadka/SBA.307.HTML.-.CSS.git",
+    siteUrl: "https://f12024-hub.netlify.app/",
   },
 ];
 
